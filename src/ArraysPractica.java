7f20f129d76332400b2511641fa5a724ffa824bc
jruanova1987 <jorge.r@saffire.com>
2023-05-12 08:34:09 -0300

    while y do while

---
+++ src/ArraysPractica.java
@@ -0,0 +1,16 @@
+public class ArraysPractica {
+
+    //    psvm para crear clase rapida
+    public static void main(String[] args) {
+
+        String[] frutas = {"manzana", "banana", "pera", "naranja"};
+
+        for (String element : frutas) {
+            System.out.println("fruta: "+element);
+        }
+
+        System.out.println("-----------------");
+        System.out.println("la primer fruta es " + frutas[0]+"\nla segunda fruta es " + frutas[1]+"\nla tercer fruta es " + frutas[2]+"\nla cuarta fruta es " + frutas[3]);
+
+    }
+}
