f804f1f1697e2ca19e1dd3d9344843b10a064849
jruanova1987 <jorge.r@saffire.com>
2023-05-12 08:20:04 -0300

    while y do while

---
+++ src/TestingLoops.java
@@ -0,0 +1,17 @@
+public class TestingLoops {
+    public static void main(String[] args) {
+        int nroLoop = 0;
+//        WHILE---------------
+//        while (nroLoop <= 10) {
+//            System.out.println("el nuevo con While numero es: " + nroLoop);
+//            nroLoop++;
+//        }
+
+//        DO WHILE---------------
+        do {
+            nroLoop++;
+            System.out.println("el nuevo con Do While numero es: " + nroLoop);
+
+        } while (nroLoop < 10);
+    }
+}
