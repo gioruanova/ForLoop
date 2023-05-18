8df18d074cc42ff409542d583c8d4ab098ed8a32
jruanova1987 <jorge.r@saffire.com>
2023-05-17 21:46:56 -0300

    nuevo-desfio-capicua

--- src/DesafioNumeroCuatroDigitos.java
+++ src/DesafioNumeroCuatroDigitos.java
@@ -3,30 +3,22 @@ import javax.swing.*;
 public class DesafioNumeroCuatroDigitos {
     public static void main(String[] args) {
 
-        String uni;
-        String dec;
-        String cen;
-        String mil;
-        String nro_ingresado;
-        int nroValidador;
-
-        nro_ingresado = JOptionPane.showInputDialog(null, "Ingrese un número de 4 dígitos:");
-        nroValidador = Integer.parseInt(nro_ingresado);
+        String nro_ingresado = JOptionPane.showInputDialog(null, "Ingrese un número de 4 dígitos:");
+        int nroValidador = Integer.parseInt(nro_ingresado);
 
         while (nroValidador <= 999 || nroValidador > 9999) {
 
-            nro_ingresado = JOptionPane.showInputDialog(null, "2 - Ingrese un número de 4 dígitos:");
+            nro_ingresado = JOptionPane.showInputDialog(null, "El numero debe ser entre 999 y 9999.\n" +
+                    "Ingrese un número de 4 dígitos:");
             nroValidador = Integer.parseInt(nro_ingresado);
         }
 
-
-        mil = String.valueOf(nro_ingresado.charAt(0));
-        cen = String.valueOf(nro_ingresado.charAt(1));
-        dec = String.valueOf(nro_ingresado.charAt(2));
-        uni = String.valueOf(nro_ingresado.charAt(3));
+        String mil = String.valueOf(nro_ingresado.charAt(0));
+        String cen = String.valueOf(nro_ingresado.charAt(1));
+        String dec = String.valueOf(nro_ingresado.charAt(2));
+        String uni = String.valueOf(nro_ingresado.charAt(3));
 
         if (mil.equals(uni) && dec.equals(cen)) {
-            System.out.println("El numero es capicua");
             JOptionPane.showMessageDialog(null, "El numero es CAPICUA");
         } else {
             JOptionPane.showMessageDialog(null, "El numero NO es CAPICUA");
