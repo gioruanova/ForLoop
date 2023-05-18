47fce2cafadfb9dfd6d796236fda766c62fc749c
jruanova1987 <jorge.r@saffire.com>
2023-05-17 21:36:14 -0300

    nuevo-desfio-capicua

--- src/DesafioNumeroCuatroDigitos.java
+++ src/DesafioNumeroCuatroDigitos.java
@@ -3,27 +3,35 @@ import javax.swing.*;
 public class DesafioNumeroCuatroDigitos {
     public static void main(String[] args) {
 
-        String uni = "";
-        String dec = "";
-        String cen = "";
-        String mil = "";
-        String nro_ingresado = JOptionPane.showInputDialog(null, "ingresar numero de 4 digitos");
+        String uni;
+        String dec;
+        String cen;
+        String mil;
+        String nro_ingresado;
+        int nroValidador;
 
-        if (nro_ingresado.length() == 4) {
-            mil = String.valueOf(nro_ingresado.charAt(0));
-            cen = String.valueOf(nro_ingresado.charAt(1));
-            dec = String.valueOf(nro_ingresado.charAt(2));
-            uni = String.valueOf(nro_ingresado.charAt(3));
+        nro_ingresado = JOptionPane.showInputDialog(null, "Ingrese un número de 4 dígitos:");
+        nroValidador = Integer.parseInt(nro_ingresado);
 
-            if (mil.equals(uni) && dec.equals(cen)) {
-                System.out.println("El numero es capicua");
-            } else {
-                System.out.println("El numero no es capicua");
-            }
+        while (nroValidador <= 999 || nroValidador > 9999) {
+            nroValidador = Integer.parseInt(nro_ingresado);
+            nro_ingresado = JOptionPane.showInputDialog(null, "2 - Ingrese un número de 4 dígitos:");
+            nroValidador = Integer.parseInt(nro_ingresado);
+        }
 
-        } else if (nro_ingresado.length() < 4) {
-            System.out.println("El numero debe ser de 4 digitos");
+
+        mil = String.valueOf(nro_ingresado.charAt(0));
+        cen = String.valueOf(nro_ingresado.charAt(1));
+        dec = String.valueOf(nro_ingresado.charAt(2));
+        uni = String.valueOf(nro_ingresado.charAt(3));
+
+        if (mil.equals(uni) && dec.equals(cen)) {
+            System.out.println("El numero es capicua");
+            JOptionPane.showMessageDialog(null, "El numero es CAPICUA");
+        } else {
+            JOptionPane.showMessageDialog(null, "El numero NO es CAPICUA");
         }
     }
 }
+
 
